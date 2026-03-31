86b599ff7b89dea93974e2eb4284c38ca9340c68
ivmp80 <ivmp80@gmail.com>
2026-03-31 22:40:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from datetime import datetime
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
@@ -322,4 +323,6 @@ def main():
 
 
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     main()
